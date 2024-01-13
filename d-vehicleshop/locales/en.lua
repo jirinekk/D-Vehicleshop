d042d1f3c60dc0f42963ddd993600ef82d95996f
jirinekk <150613466+jirinekk@users.noreply.github.com>
2024-01-13 17:05:06 +0100

    Add files via upload

---
+++ d-vehicleshop/locales/en.lua
@@ -0,0 +1,32 @@
+Locales['en'] =
+{
+    ['vehicle'] = "<font face='Fire Sans'>Vozidlo",
+    ['vehicletext'] = "<font face='Fire Sans'>Stisknìte ~r~%s~w~ pro pøístup k obchodu s vozidly <br> Stisknìte ~b~%s~w~ pro prodej vozidla",
+    ['purchased'] = "<font face='Fire Sans'>Koupili jste %s za %s%s",
+    ['sold'] = "<font face='Fire Sans'>Prodali jste své %s za %s%s",
+    ['missingmoney'] = "<font face='Fire Sans'>Nemáte dost penìz na svém bankovním úètu. Potøebujete %s%s navíc.",
+    ['remainingtime'] = "<font face='Fire Sans'>Zbývající èas: %s sekund",
+    ['nospotclear'] = "<font face='Fire Sans'>Není žádné volné místo, kde by mohlo vozidlo spawnout",
+    ['plateisalreadyused'] = "<font face='Fire Sans'>Tabulka %s je již používána",
+    ['cantsellthisvehicle'] = "<font face='Fire Sans'>Nemùžete prodat toto vozidlo",
+    ['novehiclefound'] = "<font face='Fire Sans'>V dosahu není žádné vozidlo. Zkuste to prosím pozdìji.",
+    -- Test drive
+    ["testdrive:start"] = "<font face='Fire Sans'>Váš testovací jízda zaèala. Nyní máte %s sekund",
+    ["testdrive:stop"] = "<font face='Fire Sans'>Váš testovací jízda skonèila.",
+    ["distancetobig:warning"] = "<font face='Fire Sans'>Prosím, nejezdìte dál. Toto je vaše první a poslední varování. Pokud pojedete dál, váš testovací jízda skonèí",
+    ["distancetobig:end"] = "<font face='Fire Sans'>Vzdálenost byla pøíliš velká. Váš testovací jízda skonèila",
+    -- Kategorie
+    ["sports"] = "<font face='Fire Sans'>Sportovní",
+    ["super"] = "<font face='Fire Sans'>Superautá",
+    ["suv"] = "<font face='Fire Sans'>SUV",
+    ["coupes"] = "<font face='Fire Sans'>Kupé",
+    ["compacts"] = "<font face='Fire Sans'>Kompaktní",
+    ["sedans"] = "<font face='Fire Sans'>Sedany",
+    ["motos"] = "<font face='Fire Sans'>Motocykly",
+    ["muscle"] = "<font face='Fire Sans'>Svalová auta",
+    ["classic"] = "<font face='Fire Sans'>Klasika",
+    ["offroad"] = "<font face='Fire Sans'>Terénní",
+    ["vans"] = "<font face='Fire Sans'>Dodávky",
+    -- Update
+    ['vehicletext2'] = "<font face='Fire Sans'>Stisknìte ~r~%s~w~ pro pøístup k obchodu s vozidly",
+}
